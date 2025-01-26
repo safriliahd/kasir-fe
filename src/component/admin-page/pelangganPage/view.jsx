18c2042172fec44fb5c981755d10a2305210ee0e
safriliahd <safrilianurhidayah@gmail.com>
2025-01-26 14:39:20 +0700

    (ketinggalan)

--- src/component/admin-page/pelangganPage/view.jsx
+++ src/component/admin-page/pelangganPage/view.jsx
@@ -1,13 +1,35 @@
-// export default function PelangganPage() {
-//     (
-//         <>
-//             <h2>admin pelanggan page</h2>
-//         </>
-//     )
-// }
+import { Link } from 'react-router-dom';
+import GetAllPelanggan from './allPelanggan/view';
+import { Button, Box, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add'; // Icon untuk tombol Add
+import { light, teal } from '../../../theme/color';
 
 export default function PelangganPage() {
     console.log("Rendering PelangganPage");
-    return <div>PelangganPage Content</div>;
-  }
-  
+    
+    return (
+        <>
+            <Box display="flex" justifyContent="space-between" alignItems="center" p={2}>
+            <Typography variant="h4" gutterBottom sx={{fontWeight: 'bold'}}>Daftar Pelanggan</Typography>
+                <Link to="/admin/add-pelanggan" style={{ textDecoration: 'none' }}>
+                    <Button 
+                        variant="contained" 
+                        color="teal" 
+                        startIcon={<AddIcon />} 
+                        sx={{
+                            backgroundColor: teal[500],
+                            color: light[200], // Teks warna putih
+                            '&:hover': {
+                                backgroundColor: teal[300], // Warna background saat hover
+                                color: light[100], // Teks tetap putih saat hover
+                            }
+                        }}
+                    >
+                        Add Pelanggan
+                    </Button>
+                </Link>
+            </Box>
+            <GetAllPelanggan />
+        </>
+    );
+}
